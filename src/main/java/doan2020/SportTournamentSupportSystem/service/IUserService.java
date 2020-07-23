8b4b07a4a92def113445ac7d9f1759f8c8534c90
cong-coc-2612 <congcoicoc2612@gmail.com>
2020-07-24 00:39:54 +0700

    [Cong] Reset Master

--- src/main/java/doan2020/SportTournamentSupportSystem/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package doan2020.SportTournamentSupportSystem.service;
-
-import org.springframework.data.domain.Pageable;
-
-import doan2020.SportTournamentSupportSystem.response.Response;
-
-public interface IUserService {
-	Response findAll(Pageable pageable);
-}
