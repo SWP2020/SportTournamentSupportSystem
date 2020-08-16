ae6536301ada2cd00cc4ff9d3981075407482d43
marmcohuyreus <quanghuy6698@gmail.com>
2020-08-17 03:05:23 +0700

    [huy][17/8/2020] demo unit test: login api

--- src/test/java/doan2020/SportTournamentSupportSystem/testApi/TestLoginAPI.java
+++ src/test/java/doan2020/SportTournamentSupportSystem/testApi/TestLoginAPI.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package doan2020.SportTournamentSupportSystem.testApi;
 
 import java.util.Date;
@@ -20,7 +19,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import doan2020.SportTournamentSupportSystem.api.LoginAPI;
 import doan2020.SportTournamentSupportSystem.converter.UserConverter;
-import doan2020.SportTournamentSupportSystem.dtIn.LoginDtIn;
+import doan2020.SportTournamentSupportSystem.dto.UserDTO;
 import doan2020.SportTournamentSupportSystem.entity.UserEntity;
 import doan2020.SportTournamentSupportSystem.repository.UserRepository;
 import doan2020.SportTournamentSupportSystem.response.Response;
@@ -126,7 +125,9 @@ public class TestLoginAPI {
 		//phần data test (thay đổi theo các test case tương ứng)
 		String username = "Cong";
 		String password = "123456";
-		LoginDtIn user = new LoginDtIn(username, password);
+		UserDTO user = new UserDTO();
+		user.setUsername(username);
+		user.setPassword(password);
 		
 		//phần expected result
 		HttpStatus expectedHttpStatus = HttpStatus.OK;
@@ -146,72 +147,3 @@ public class TestLoginAPI {
 	}
 	
 }
-=======
-package doan2020.SportTournamentSupportSystem.testApi;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import doan2020.SportTournamentSupportSystem.api.LoginAPI;
-import doan2020.SportTournamentSupportSystem.converter.UserConverter;
-import doan2020.SportTournamentSupportSystem.dto.UserDTO;
-import doan2020.SportTournamentSupportSystem.response.Response;
-import doan2020.SportTournamentSupportSystem.service.IVerificationTokenService;
-import doan2020.SportTournamentSupportSystem.service.impl.JwtService;
-import doan2020.SportTournamentSupportSystem.service.impl.LoginService;
-import doan2020.SportTournamentSupportSystem.service.impl.UserService;
-
-@RunWith(SpringRunner.class)
-public class TestLoginAPI {
-	
-	@InjectMocks
-	LoginAPI loginApi;
-	
-	@Mock
-	LoginService loginService;
-	
-	@Mock
-	BCryptPasswordEncoder passwordEncoder;
-
-	@Mock
-	JwtService jwtService;
-
-	@Mock
-	UserService userService;
-
-	@Mock
-	UserConverter converter;
-
-	@Mock
-	IVerificationTokenService verificationTokenService;
-	
-	@Test
-	public void testLogin() {
-		//phần data test (thay đổi theo các test case tương ứng)
-
-		String username = "Cong";
-		String password = "123456";
-		UserDTO user = new UserDTO();
-		user.setUsername(username);
-		user.setPassword(password);
-		
-		//phần expected result
-		HttpStatus expectedHttpStatus = HttpStatus.OK;
-		
-		//phần execute test
-		ResponseEntity<Response> response = loginApi.login(user);
-		
-		HttpStatus actualHttpStatus = response.getStatusCode();
-		System.out.println(actualHttpStatus);
-		Assert.assertEquals(expectedHttpStatus, actualHttpStatus);
-	}
-	
-}
->>>>>>> branch 'develop' of https://github.com/SWP2020/SportTournamentSupportSystem.git
