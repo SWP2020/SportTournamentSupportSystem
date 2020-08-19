296d5e86a80635faa49c80eb4179349055e803f4
cong-coc-2612 <congcoicoc2612@gmail.com>
2020-08-19 16:25:41 +0700

    [Cong] fix jwtService

--- src/main/java/doan2020/SportTournamentSupportSystem/service/impl/JwtService.java
+++ src/main/java/doan2020/SportTournamentSupportSystem/service/impl/JwtService.java
@@ -6,15 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.JWSSigner;
-import com.nimbusds.jose.JWSVerifier;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jose.crypto.MACVerifier;
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.SignedJWT;
-
 import doan2020.SportTournamentSupportSystem.entity.UserEntity;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -36,24 +27,6 @@ public class JwtService {
 	public String generateTokenLogin(UserEntity user) {
 		String token = null;
 		try {
-//			// Create HMAC signer
-//			JWSSigner signer = new MACSigner(generateShareSecret());
-//
-//			JWTClaimsSet.Builder builder = new JWTClaimsSet.Builder();
-////			builder.claim(USERNAME, user.getUsername());
-//			Long id = (long) 1;
-//			builder.claim("id", id);
-//			builder.expirationTime(generateExpirationDate());
-//
-//			JWTClaimsSet claimsSet = builder.build();
-//			SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
-//
-//			// Apply the HMAC protection
-//			signedJWT.sign(signer);
-//
-//			// Serialize to compact form, produces something like
-//			// eyJhbGciOiJIUzI1NiJ9.SGVsbG8sIHdvcmxkIQ.onO9Ihudz3WkiauDO2Uhyuz0Y18UASXlSc1eS0NkWyA
-//			token = signedJWT.serialize();
 			
 			return Jwts.builder()
 					.setSubject((user.getUsername()))
