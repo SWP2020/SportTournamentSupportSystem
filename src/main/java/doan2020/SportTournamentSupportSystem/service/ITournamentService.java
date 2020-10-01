53e7837a11bb7f9dda20b87063078483297f0b5a
cong-coc-2612 <congcoicoc2612@gmail.com>
2020-10-01 12:16:00 +0700

    [cong] fix search_tournament

--- src/main/java/doan2020/SportTournamentSupportSystem/service/ITournamentService.java
+++ src/main/java/doan2020/SportTournamentSupportSystem/service/ITournamentService.java
@@ -8,34 +8,37 @@ import org.springframework.data.domain.Pageable;
 import doan2020.SportTournamentSupportSystem.entity.TournamentEntity;
 
 public interface ITournamentService {
-	
-	public TournamentEntity findOneById(Long id);
-	
-	public Collection<TournamentEntity> findAll(Pageable pageable);
-	
-	public TournamentEntity create(TournamentEntity tournamentEntity);
-	
-	public TournamentEntity update(Long id, TournamentEntity newEntity);
-	
-	public TournamentEntity updateAvatar(Long id, TournamentEntity newEntity);
-	
-	public TournamentEntity updateBackground(Long id, TournamentEntity newEntity);
-	
-//	public Collection<TournamentEntity> findAll();
-	
-	public TournamentEntity delete(Long id);
-	
-	public Collection<TournamentEntity> findByCreatorId(Pageable pageable, Long creatorId);
-	
-	public Collection<TournamentEntity> findBySearchString(Pageable pageable, String searchString);
-	
-	public Long countBySearchString(String searchString);
-	
-	public Long countAll();
-	
-	public Map<String, Object> getOtherInformation(Long Id);
-	
-	public TournamentEntity updateStatus(TournamentEntity newEntity, String status); 
 
+	public TournamentEntity findOneById(Long id);
+
+	public Collection<TournamentEntity> findAll(Pageable pageable);
+
+	public TournamentEntity create(TournamentEntity tournamentEntity);
+
+	public TournamentEntity update(Long id, TournamentEntity newEntity);
+
+	public TournamentEntity updateAvatar(Long id, TournamentEntity newEntity);
+
+	public TournamentEntity updateBackground(Long id, TournamentEntity newEntity);
+
+//	public Collection<TournamentEntity> findAll();
+
+	public TournamentEntity delete(Long id);
+
+	public Collection<TournamentEntity> findByCreatorId(Pageable pageable, Long creatorId);
+
+	public Collection<TournamentEntity> findBySearchString(Pageable pageable, String searchString);
+
+	public Long countBySearchString(String searchString);
+
+	public Long countAll();
+
+	public Map<String, Object> getOtherInformation(Long Id);
+
+	public TournamentEntity updateStatus(TournamentEntity newEntity, String status);
+
+	public Collection<TournamentEntity> findBySearchStringAndSportAndStatus(Pageable pageable, String searchString, String sport, String status);
+
+	public Long countBySearchStringAndSportAndStatus(String searchString, String sport, String status);
 
 }
